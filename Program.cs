e54eb2bded2c5f30d79d884fe16122126cd84da2
siba98 <gyansiba.patro@gmail.com>
2021-09-14 20:24:16 +0530

    Siba: added class compilation file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day4_SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Problem");
+
+            //UC1_StartPosition.start();
+            //UC2_RandomDice.DiceRoll();
+            //UC3_CheckPosition.PositionCheck();
+            UC4_WinningPosition.WinnerPosition();
+        }
+    }
+}
