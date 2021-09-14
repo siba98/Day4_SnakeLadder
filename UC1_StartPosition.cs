9f38cedee54bf14e3a409be4afc0867b9883dbac
siba98 <gyansiba.patro@gmail.com>
2021-09-14 20:28:52 +0530

    Siba: added the start position as 0

---
+++ UC1_StartPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4_SnakeLadder
+{
+    class UC1_StartPosition
+    {
+        public static void start ()
+        {
+            int startPosition = 0;
+            Console.WriteLine("The starting position is: " + startPosition);
+        }
+    }
+}
