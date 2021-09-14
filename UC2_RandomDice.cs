c6914d6b3151196173ec2e531608f3699700c6c6
siba98 <gyansiba.patro@gmail.com>
2021-09-14 20:29:58 +0530

    Siba: added a random dice number

---
+++ UC2_RandomDice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4_SnakeLadder
+{
+    class UC2_RandomDice
+    {
+        public static void DiceRoll ()
+        {
+            int player1;
+            Random rd = new Random();
+            player1 = rd.Next(1, 7);
+            Console.WriteLine("The player1 gets the random dice number is: " + player1);
+
+        }
+    }
+}
