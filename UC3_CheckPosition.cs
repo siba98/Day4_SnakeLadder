1350494132dfe86d034b3b444059b00fd3c569e4
siba98 <gyansiba.patro@gmail.com>
2021-09-16 21:43:50 +0530

    Siba: added position check of player

---
+++ UC3_CheckPosition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4_SnakeLadder
+{
+    class UC3_CheckPosition
+    {   
+        public static void PositionCheck()
+        {
+            int no_Play = 0;
+            int ladder = 1;
+            int snake = 2;
+            //variables
+            int playerPosition = 0;
+            Random ran = new Random();
+            int optionCheck = ran.Next(0, 3);
+            int diceValue = ran.Next(0, 7);
+
+            switch (optionCheck)
+            {
+                case 0:
+                    Console.WriteLine("No play, pass the dice");
+                    break;
+                case 1:
+                    playerPosition += diceValue;
+                    Console.WriteLine("You got ladder " + "and your position is now: " + playerPosition);
+                    break;
+                case 2:
+                    if (playerPosition <= 0)
+                    {
+                        Console.WriteLine("U got snake but");
+                        Console.WriteLine("You are still in Zero(0) position");
+                    }
+                    break;
+            }
+        }
+    }
+}
