e5b69d7d1a85603e688ea7fa32b508ac880cd56c
siba98 <gyansiba.patro@gmail.com>
2021-09-16 23:49:43 +0530

    refactor

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ namespace Day4_SnakeLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Problem");
+            Console.WriteLine();
 
-            //UC1_StartPosition.start();
-            //UC2_RandomDice.DiceRoll();
-            //UC3_CheckPosition.PositionCheck();
+            UC1_StartPosition.start();
+            UC2_RandomDice.DiceRoll();
+            UC3_CheckPosition.PositionCheck();
             UC4_WinningPosition.WinnerPosition();
+            UC5_ExactWinningPosition.ExactWinning();
+            UC6_NumOfDice.TimesPlayed();
+            UC7_TwoPlayer.playingTwoPlayer();
         }
     }
 }
