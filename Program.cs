f7641328ca1279cd09ac983036a0d6856c56e5c4
siba98 <gyansiba.patro@gmail.com>
2021-09-24 21:01:56 +0530

    Siba:[Refactored] the main method

--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ namespace Day4_SnakeLadder
             Console.WriteLine("Welcome to Snake and Ladder Problem");
             Console.WriteLine();
 
-            UC1_StartPosition.start();
-            UC2_RandomDice.DiceRoll();
-            UC3_CheckPosition.PositionCheck();
-            UC4_WinningPosition.WinnerPosition();
-            UC5_ExactWinningPosition.ExactWinning();
-            UC6_NumOfDice.TimesPlayed();
-            UC7_TwoPlayer.playingTwoPlayer();
+            //UC1_StartPosition.start();
+            //UC2_RandomDice.DiceRoll();
+            //UC3_CheckPosition.PositionCheck();
+            //UC4_WinningPosition.WinnerPosition();
+            //UC5_ExactWinningPosition.ExactWinning();
+            //UC6_NumOfDice.TimesPlayed();
+            UC7_TwoPlayer.TwoPlayer();
         }
     }
 }
